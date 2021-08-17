8cc34d4492ae61614237bd2fc342ec486fde7760
luke-chant <luke.chant@autotrader.co.uk>
2021-08-17 14:41:24 +0100

    add challenge 8 solution

--- challenge-8.js
+++ challenge-8.js
@@ -1,26 +1,29 @@
 {
     init: function(elevators, floors) {
         elevators.forEach(elevator => {
+            elevator.destinationQueue = [];
             elevator.on("idle", function() { 
-                elevator.goToFloor(0);
                 elevator.goToFloor(Math.floor(floors.length / 2));
             });
 
-            elevator.on("floor_button_pressed", function(floorNum) { 
-                elevator.goToFloor(floorNum);
-            } );
-
-            function pressDirection(floor) {
+            floors.forEach(floor => {
                 floor.on("up_button_pressed", function() {
                     elevator.destinationQueue.sort(function(a, b){return b-a});
                     elevator.checkDestinationQueue();
                     elevator.goToFloor(floor.floorNum());
                 });
                 floor.on("down_button_pressed", function() {
+                    elevator.checkDestinationQueue();
                     elevator.goToFloor(floor.floorNum());
                 });
-            }
+                elevator.on("floor_button_pressed", function(floorNum) { 
+                    elevator.checkDestinationQueue();
+                    elevator.goToFloor(floorNum);
+                } );
+            })
         })
+        
+        
     },
         update: function(dt, elevators, floors) {
         }
